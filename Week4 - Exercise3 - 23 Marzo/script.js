fc2004fd67814dcb1ac20fee4821a632aa3ae1de
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-03-23 22:08:37 +0100

    fit: card change on click and show description

--- Week4 - Exercise3 - 23 Marzo/script.js
+++ Week4 - Exercise3 - 23 Marzo/script.js
@@ -21,11 +21,10 @@ const createCard = (data) => {
 
   // Element creation
   wrapper.innerHTML =
-  data.map((obj, index) => `<div>
+  data.map((obj, index) => `<div data-desc="${obj.description}">
   <img class="deleteBtn" id="${index}" src="closeBtn.png">
   <h2>${obj.title.split("").slice(0, 51).join("")}</h2>
-  <img src="${obj.image}" nmjb>
-  <p data-desc="${obj.description}>${obj.description}</p>
+  <img src="${obj.image}">
   <p class="price">${obj.price} €</p>
   </div>`);
   
@@ -41,18 +40,21 @@ const createCard = (data) => {
       createCard(data);
     });
 
-    // Description on click
-    
-    // const cards = document.querySelectorAll(".wrapper>div");
-    // cards.forEach((card) => {
-    //   card.addEventListener("click", () => {
-    //     card.classList.add("animated");
-    //     card.innerHTML =`<p>${card.getAttribute("data-desc")}</p>`;
-    //   })
-    // })
   });
-
-
+  
+  // Description on click
+  
+  const cards = document.querySelectorAll(".wrapper>div");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      card.classList.toggle("swipe");
+      const oldContent = card.innerHTML;
+      card.innerHTML = `<p>${card.getAttribute("data-desc")}</p>`;
+      card.addEventListener("click", () => {
+        card.innerHTML = oldContent;
+      })
+    })
+  })
 
 }
 
