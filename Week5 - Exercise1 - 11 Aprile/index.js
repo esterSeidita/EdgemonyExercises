5f72fc33d192929589a2ed830f53ff00fca148c0
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-04-11 20:36:10 +0200

    feat: updated exercise with html file generator

--- Week5 - Exercise1 - 11 Aprile/index.js
+++ Week5 - Exercise1 - 11 Aprile/index.js
@@ -1,21 +1,33 @@
 const fs = require("fs").promises;
 
 async function WriteFile(path, content){
-    await fs.writeFile(path, content, {flag: "a+"});
+    await fs.writeFile(path, content);
 }
 
-async function ReadFile(file){
-    const result = await fs.readFile(file);
-    console.log(result.toString());
-}
+// async function ReadFile(file){
+//     const result = await fs.readFile(file);
+//     console.log(result.toString());
+// }
 
 async function WriteAndRead(path, content){
     WriteFile(path, content);
-    ReadFile(path);
+    // ReadFile(path);
 }
 
 const arguments = process.argv.slice(2);
 
-WriteAndRead("./spesa.txt", `${arguments}\n`)
+WriteAndRead("./index.html", `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lista</title>
+    </head>
+    <body>
+        <ul>
+            ${arguments.map((item) => `<li>${item}</li>`).join("")}
+        </ul>
+    </body>
+</html>
+`)
 
 
